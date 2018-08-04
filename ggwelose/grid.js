e828661f880aca5ba5383ef8b40965e6bc0dd947
popados <popados@gmail.com>
2018-08-03 22:35:57 -0500

    first

---
+++ ggwelose/grid.js
@@ -0,0 +1,108 @@
+
+var canvas = document.getElementById('testArea');
+var ctx = canvas.getContext("2d");
+
+
+//created a box and function to make one row of 16x16 pixels
+var box = {
+  boxPixel_X : 0,
+  boxPixel_Y : 0,
+  global_pixel_X : 16,
+  global_pixel_Y : 16,
+};
+// var player = {
+//
+//
+// };
+var arr_values = [];
+var one_box = box;
+var box_X = one_box.boxPixel_X;
+var box_Y = one_box.boxPixel_Y;
+
+
+for (box_Y= 0; box_Y <= 720; box_Y +=32){
+  arr_values.push(box_Y);
+}
+
+var y_value = 0;
+//draw one row
+function drawRow(y_value){
+  //console.log(arr_values);
+  for (box_X=0; box_X <= 1020; box_X +=32){
+    ctx.beginPath();
+    ctx.rect(box_X, y_value, 32, 32);
+    ctx.strokeStyle = "rgba(255, 0, 0, 1)";
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+//draws the rest of the rows with new y values
+function drawEachRow(){
+  for (y_value = 0; y_value <=720; y_value+=32){
+    drawRow(y_value);
+  }
+}
+
+drawEachRow();
+
+
+
+// function drawRow2(y_value){
+//   for (box_X= 0; box_X <= 1280; box_X +=16){
+//     ctx.beginPath();
+//     ctx.rect(box_X, y_value, 16, 16);
+//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
+//     ctx.stroke();
+//     ctx.closePath();
+//   }
+// }
+
+//drawRow(y_value);
+//drawRow2();
+// function drawRow2(){
+//   for (var box_X; box_X <= 1280; box_X +=16){
+//     ctx.beginPath();
+//     ctx.rect(box_X, 0, 16, 16);
+//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
+//     ctx.stroke();
+//     ctx.closePath();
+//   }
+// }
+//
+
+// function row_two(){
+//   for (box_X; box_X <= 1280; box_X +=16){
+//     ctx.beginPath();
+//     ctx.rect(box_X, 16, 16, 16);
+//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
+//     ctx.stroke();
+//     ctx.closePath();
+//   }
+// }
+
+//
+// function drawNext(box_Y){
+//   for (box_Y; box_Y <= 720; box_Y +=16){
+//     drawRow(box_Y);
+//   }
+// }
+// drawNext(box_Y);
+// drawRow();
+// row_two();
+//
+// function changeY(){
+//   for (box_Y = 16; box_Y <= 720; box_Y +=16){
+//     ctx.beginPath();
+//     ctx.rect(box_X, box_Y, 16, 16);
+//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
+//     ctx.stroke();
+//     ctx.closePath();
+//   }
+// }
+//
+// function draw() {
+//   drawRow();
+//   changeY();
+// }
+
+//draw();
---
+++ ggwelose/player.js
@@ -0,0 +1,58 @@
+
+var canvas = document.getElementById('testArea');
+var ctx = canvas.getContext("2d");
+
+//
+var player = {
+  xpos : 0,
+  ypos : 0,
+};
+
+var x_pos = player.xpos;
+var y_pos = player.ypos;
+
+function hi (){
+  alert("hi");
+  ctx.fillStyle = "#0ff";
+  ctx.fillRect(x_pos,y_pos,32,32);
+}
+
+function move(e){
+  console.log(e.keyCode);
+  if(e.keyCode === 83){
+    y_pos +=32;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawEachRow();
+  }
+  if(e.keyCode === 68){
+    x_pos += 32;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawEachRow();
+  }
+  if(e.keyCode === 87){
+    y_pos -= 32;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawEachRow();
+
+  }
+  if(e.keyCode === 65){
+    x_pos -= 32;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawEachRow();
+  }
+  // ctx.fillStyle = "#0ff";
+  ctx.fillRect(x_pos,y_pos,32,32);
+
+  // ctx.stroke();
+}
+hi();
+document.onkeydown = move;
+//
+//
+// function update(time = 0){
+//   const deltaTime = time - lastTime;
+//   lastTime = time;
+//   console.log(lastTime);
+//
+// }
+// update();
