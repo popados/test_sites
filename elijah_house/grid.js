1fc488a0f07a7345a43780155b26bfa3a886df9c
popados <popados@gmail.com>
2018-08-06 16:57:46 -0500

    added movement

--- elijah_house/grid.js
+++ elijah_house/grid.js
@@ -1,6 +1,6 @@
 
 var canvas = document.getElementById('testArea');
-var ctx = canvas.getContext("2d");
+var context = canvas.getContext("2d");
 
 
 //created a box and function to make one row of 16x16 pixels
@@ -10,52 +10,70 @@ var box = {
   global_pixel_X : 32,
   global_pixel_Y : 32,
 };
+//the object box can be used to init a SpriteImg
+
+//assign grid value to array_index[element]
+var one_box = box;
+var box_X = one_box.boxPixel_X;
+var box_Y = one_box.boxPixel_Y;
+var array_values = [];
+
+var y_value = 0;
+//draw one row
+function drawGrid(){
+  // context.beginPath();
+  // context.moveTo(32,0);
+  // context.lineTo(32,800);
+  // context.stroke();
+  //
+
+  context.beginPath();
+  context.rect(box_X, y_value, 32, 32);
+  context.strokeStyle = "rgba(255, 0, 0, 1)";
+
+  context.stroke();
+  context.closePath();
+
+}
+function drawRow(y_value){
+  //console.log(arr_values);
+  for (box_X=0; box_X <= 1024; box_X +=32){
+    while (box_X <=1024){
+      array_values.push(box_X);
+      break;
+    }
+    drawGrid();
+  }
+}
+// draws the rest of the rows with new y values
+function drawEachRow(){
+  for (y_value = 0; y_value <=720; y_value+=32){
+
+    drawRow(y_value);
+  }
+}
+
+console.log(array_values);
+drawEachRow();
+
 // var player = {
 //
 //
 // };
 
 
-var arr_values = [];
-var one_box = box;
-var box_X = one_box.boxPixel_X;
-var box_Y = one_box.boxPixel_Y;
-
-
-for (box_Y= 0; box_Y <= 720; box_Y +=32){
-  arr_values.push(box_Y);
-}
-
-var y_value = 0;
-//draw one row
-function drawRow(y_value){
-  //console.log(arr_values);
-  for (box_X=0; box_X <= 1020; box_X +=32){
-    ctx.beginPath();
-    ctx.rect(box_X, y_value, 32, 32);
-    ctx.strokeStyle = "rgba(255, 0, 0, 1)";
-    ctx.stroke();
-    ctx.closePath();
-  }
-}
-//draws the rest of the rows with new y values
-function drawEachRow(){
-  for (y_value = 0; y_value <=720; y_value+=32){
-    drawRow(y_value);
-  }
-}
-
-drawEachRow();
-
+// for (box_Y= 0; box_Y <= 720; box_Y +=32){
+//   arr_values.push(box_Y);
+// }
 
 
 // function drawRow2(y_value){
 //   for (box_X= 0; box_X <= 1280; box_X +=16){
-//     ctx.beginPath();
-//     ctx.rect(box_X, y_value, 16, 16);
-//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
-//     ctx.stroke();
-//     ctx.closePath();
+//     context.beginPath();
+//     context.rect(box_X, y_value, 16, 16);
+//     context.strokeStyle = "rgba(0, 50, 255, 1)";
+//     context.stroke();
+//     context.closePath();
 //   }
 // }
 
@@ -63,22 +81,22 @@ drawEachRow();
 //drawRow2();
 // function drawRow2(){
 //   for (var box_X; box_X <= 1280; box_X +=16){
-//     ctx.beginPath();
-//     ctx.rect(box_X, 0, 16, 16);
-//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
-//     ctx.stroke();
-//     ctx.closePath();
+//     context.beginPath();
+//     context.rect(box_X, 0, 16, 16);
+//     context.strokeStyle = "rgba(0, 50, 255, 1)";
+//     context.stroke();
+//     context.closePath();
 //   }
 // }
 //
 
 // function row_two(){
 //   for (box_X; box_X <= 1280; box_X +=16){
-//     ctx.beginPath();
-//     ctx.rect(box_X, 16, 16, 16);
-//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
-//     ctx.stroke();
-//     ctx.closePath();
+//     context.beginPath();
+//     context.rect(box_X, 16, 16, 16);
+//     context.strokeStyle = "rgba(0, 50, 255, 1)";
+//     context.stroke();
+//     context.closePath();
 //   }
 // }
 
@@ -94,11 +112,11 @@ drawEachRow();
 //
 // function changeY(){
 //   for (box_Y = 16; box_Y <= 720; box_Y +=16){
-//     ctx.beginPath();
-//     ctx.rect(box_X, box_Y, 16, 16);
-//     ctx.strokeStyle = "rgba(0, 50, 255, 1)";
-//     ctx.stroke();
-//     ctx.closePath();
+//     context.beginPath();
+//     context.rect(box_X, box_Y, 16, 16);
+//     context.strokeStyle = "rgba(0, 50, 255, 1)";
+//     context.stroke();
+//     context.closePath();
 //   }
 // }
 //
--- elijah_house/player_sprite.js
+++ elijah_house/player_sprite.js
@@ -1,8 +1,119 @@
+var canvas2 = document.getElementById('playerArea');
+var context1 = canvas2.getContext("2d");
+
+
 var img = new Image();
 img.src = "./images/elijah_spritesheet2.png";
 
-img.addEventListener('load', function(){
+img.onload = function(){
 
-  ctx.drawImage(img,0,0);
 
-},false);
+  // context.drawImage(img,0,up,32,32,0,0,32,32);
+  // context.drawImage(img,0,left,32,32,0,32,32,32);
+  // context.drawImage(img,0,right,32,32,0,64,32,32);
+  // context.drawImage(img,0,down,32,32,0,96,32,32);
+
+  init();
+
+
+}
+
+const width = 32;
+const height = 32;
+
+function drawFrame(frameX, frameY, canvasX, canvasY){
+
+  context1.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, width,height);
+}
+
+
+
+function init() {
+  // drawFrame(0,0,0,0);
+  // drawFrame(1,0,width,0);
+  // drawFrame(0,0,width*2,0);
+  // drawFrame(2,0,width*3,0);
+  window.requestAnimationFrame(step);
+  document.onkeydown = move;
+
+}
+
+const cycleLoop = [0, 1, 0, 2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+let spriteX = 0;
+let spriteY = 0;
+
+function step() {
+  frameCount++;
+  if(frameCount < 15){
+    window.requestAnimationFrame(step);
+    return;
+
+  }
+  frameCount = 0;
+  context1.clearRect(0, 0, width, height);
+
+  drawFrame(cycleLoop[currentLoopIndex], 0, spriteX, spriteY);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+}
+
+function move(e){
+  console.log(e.keyCode)
+  if(e.keyCode === 68){
+    spriteX += 32;
+    context1.clearRect(0,0,canvas2.width,canvas2.height);
+    drawFrame(cycleLoop[currentLoopIndex], 0, spriteX, spriteY);
+    currentLoopIndex++;
+    if (currentLoopIndex >= cycleLoop.length) {
+      currentLoopIndex = 0;
+    }
+  }
+  if(e.keyCode === 65){
+    spriteX -= 32;
+    context1.clearRect(0,0,canvas2.width,canvas2.height);
+    drawFrame(cycleLoop[currentLoopIndex], 0, spriteX, spriteY);
+    currentLoopIndex++;
+    if (currentLoopIndex >= cycleLoop.length) {
+      currentLoopIndex = 0;
+    }
+  }
+  if(e.keyCode === 87){
+    spriteY -= 32;
+    context1.clearRect(0,0,canvas2.width,canvas2.height);
+    drawFrame(cycleLoop[currentLoopIndex], 0, spriteX, spriteY);
+    currentLoopIndex++;
+    if (currentLoopIndex >= cycleLoop.length) {
+      currentLoopIndex = 0;
+    }
+  }
+  if(e.keyCode === 83){
+    spriteY += 32;
+    context1.clearRect(0,0,canvas2.width,canvas2.height);
+    drawFrame(cycleLoop[currentLoopIndex], 0, spriteX, spriteY);
+    currentLoopIndex++;
+    if (currentLoopIndex >= cycleLoop.length) {
+      currentLoopIndex = 0;
+    }
+  }
+}
+
+document.onkey
+
+
+
+
+
+
+
+
+
+
+//comments //context.drawImage(
+//        srcImage,
+//        srcX, srcY, srcWidth,srcHeight,
+//        canvasX,canvasY,spriteWidth,spriteHeight);
